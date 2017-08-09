5ad10b42ad54023cb4d32dc879e8e22c9ef5b713
Eric Jiang <ericjiang97@gmail.com>
2017-08-09 12:19:40 +1000

    Added Basic Express Framework

---
+++ index.js
@@ -0,0 +1,11 @@
+var Express = require('express');
+const app = Express();
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
+})
+
